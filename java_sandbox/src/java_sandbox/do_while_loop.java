bdad6b6a54de357e9aee5cfcf96786c63c28708d
Brian C <craft1.brian@gmail.com>
2016-12-29 20:16:16 -0600

    Java Sandbox
    Code snippets to learn java syntax.  Useful for people going from
    python to java.

---
+++ java_sandbox/src/java_sandbox/do_while_loop.java
@@ -0,0 +1,18 @@
+package java_sandbox;
+
+public class do_while_loop {
+
+	public static void main(String[] args) {
+		
+		int counter;
+		counter = 20;
+		
+		do {
+			System.out.println(counter);
+			counter++;
+		} while (counter < 10);
+		
+
+	}
+
+}
---
+++ java_sandbox/src/java_sandbox/printer.java
@@ -0,0 +1,10 @@
+package java_sandbox;
+
+public class printer {
+
+	public static void main(String[] args) {
+		System.out.println("Hey this is a simple print statement");
+
+	}
+
+}
