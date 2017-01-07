02e776659174b2ca295be16c7a2aea787e76ba31
Brian C <craft1.brian@gmail.com>
2017-01-06 21:48:22 -0600

    begun work on the breadth search, still errors in the moving piece function

--- CSC_480_Artificial_Intelligence/Assignment_1_8_Puzzle_Solver/puzzle_solver.py
+++ CSC_480_Artificial_Intelligence/Assignment_1_8_Puzzle_Solver/puzzle_solver.py
@@ -29,7 +29,17 @@ class node:
             h_cost += abs(goal.index(x) - state.index(x))
         return h_cost
     
-    
+    @staticmethod
+    def heuristic_function_self_def():
+        pass
+        # placeholder for a personal defined heuristic function
+        # likely some distance cost, such as cosine, jacard, 
+        # euclidean, pearson.
+        
+    @staticmethod 
+    def get_a_star_cost(h_cost, q_cost):
+        return h_cost + q_cost
+ 
     def __init__(self, key, state, goal_state, parent_node, left_child, right_child):
         
         self.key = key
@@ -37,33 +47,32 @@ class node:
         self.parent_node = parent_node
         self.left_child = left_child
         self.right_child = right_child
-        self.records = {}
         self.goal_state = goal_state
         
     def get_moves(self):
         
-        self.records[self.key] = [[],[]]
+        self.records = [[],[]]
         
         if self.state.index(0) == 8:
-            self.records[self.key][0].append('right')
-            self.records[self.key][1].append(self.state[7])
+            self.records[0].append('right')
+            self.records[1].append(self.state[7])
                         
         elif self.state.index(0) == 0:
-            self.records[self.key][0].append('left')
-            self.records[self.key][1].append(self.state[1])
+            self.records[0].append('left')
+            self.records[1].append(self.state[1])
             
         else:
-            self.records[self.key][0].extend(('left','right'))
-            self.records[self.key][1].extend((self.state[self.state.index(0)+1], 
+            self.records[0].extend(('left','right'))
+            self.records[1].extend((self.state[self.state.index(0)+1], 
                 self.state[self.state.index(0)-1])) 
     
     def get_heuristic_cost(self, heuristic_function):
         
         if heuristic_function == 'manhattan_distance':
-            self.records[self.key].append(node.manhattan_distance(self.state,self.goal_state))  
+            self.records.append(node.manhattan_distance(self.state,self.goal_state))  
             
         elif heuristic_function == 'heuristic_function':
-            self.records[self.key].append(node.heuristic_function(self.state,self.goal_state))
+            self.records.append(node.heuristic_function(self.state,self.goal_state))
             
         else:
             print 'There is something wrong with the distance function you entered.'
@@ -72,22 +81,20 @@ class node:
         
         if move in self.records[self.key][0]:
             
-            if move == 'left':               
-                self.state[self.state.index(0) + 1], self.state[self.state.index(0)] = \
-                    self.state[self.state.index(0)], self.state[self.state.index(0) + 1]
-                self.update_records()
-                return self.state
+            if move == 'left':
+                new_state = self.state
+                new_state[new_state.index(0) + 1], new_state[new_state.index(0)] = \
+                    new_state[new_state.index(0)], new_state[new_state.index(0) + 1]
+                return [1,2,3,8,0,4,7,6,5] #not returning the new state?
                 
             else:
-                self.state[self.state.index(0)], self.state[self.state.index(0) + 1] = \
-                    self.state[self.state.index(0) - 1], self.state[self.state.index(0)]
-                self.update_records()
-                return self.state
+                new_state[new_state.index(0)], new_state[new_state.index(0) + 1] = \
+                    new_state[new_state.index(0) - 1], new_state[new_state.index(0)]
+                return [1,2,3,8,0,4,7,6,5] #not returning the new state?
                 
         else:
             return 'In the given state a {enter_move} is not possible.'.format(enter_move = move)
-        
-        
+             
 class stack:
 
     def __init__(self, init_state):
@@ -102,7 +109,6 @@ class stack:
         
     def reset_state(self, key):
         self.init_state = key
-
 
 class queue:
     
@@ -120,27 +126,58 @@ class queue:
         self.init_state = key
 
  
-class searchTree:
+class searchTreeSolver:
+        
+    def __init__(self, node):
+        
+        self.current_node = node
+        self.goal_state = node.goal_state
+        self.current_state = node.state
+        
+        self.structure = {}
+        self.structure[0] = node.state
+        self.key_counter = 1
+        
+    def breadth_first(self):
+        #(self, key, state, goal_state, parent_node, left_child, right_child)
+        
+        if self.current_state != self.goal_state:
+                        
+            move =  self.current_node.records[0][self.current_node.records[1].index \
+                (min(self.current_node.records[1]))]
+                
+            state = self.current_node.move_piece(move)
+            
+            new_node = node(self.key_counter,state,self.goal_state,self.key_counter - 1,None,None)
+            print new_node.state
+            self.current_node = new_node
+            
+            self.structure[self.key_counter] = self.current_node.state
+            
+        else:
+            return 'Omg this fucking things worked!'
+            
+
+            
+easy = [1,3,4,8,6,2,7,0,5]      
+goal = [1,2,3,8,0,4,7,6,5]             
+test_node = node(0,easy,goal,None,None,None)
+test_node.get_moves()
+test_node.get_heuristic_cost('heuristic_function')
+
+test_tree = searchTreeSolver(test_node)
+test_tree.current_node.records
+test_tree.breadth_first()
+
+import pandas as pd
+for k,v in test_tree.structure.items():
+    print '\n'
+    print 'Move Count: ', k, '\n'
+    print pd.DataFrame([[v[0],v[1],v[2]], \
+                        [v[3],v[4],v[5]], \
+                        [v[6],v[7],v[8]]]).to_string(index = False, header = False)
     
 
-
-
-
-
-
-
-
-
-
-      
-easy = [1,3,4,8,6,2,7,0,5]      
-goal = [1,2,3,8,0,4,7,6,5]
-
-
-test_node = node(1, easy, goal, None, None, None)
-test_node.get_moves()
-test_node.get_heuristic_cost('manhattan_distance')
-test_node.records
 
 
 
