c7b9518ee039d7ebc6f59d3220d2c477608749f7
Brian C <craft1.brian@gmail.com>
2017-01-07 20:35:44 -0600

    Updates to the node class
    In the move_pieces method, altered the function to not save over the
    self.state by making a new variable instead of a new reference to the
    same pointer.

--- CSC_480_Artificial_Intelligence/Assignment_1_8_Puzzle_Solver/puzzle_solver.py
+++ CSC_480_Artificial_Intelligence/Assignment_1_8_Puzzle_Solver/puzzle_solver.py
@@ -1,15 +1,5 @@
-'''
-Depaul CSC 480 Assignment: 8 Puzzle Solver
-
-Goal:   [1,2,3,8,0,4,7,6,5]
-Easy:   [1,3,4,8,6,2,7,0,5]
-Medium: [2,8,1,0,4,3,7,6,5]
-Hard:   [5,6,7,4,0,8,3,2,1]
-
-'''
-
 class node:
-    
+        
     @staticmethod 
     def heuristic_function(state, goal):
         
@@ -48,23 +38,23 @@ class node:
         self.left_child = left_child
         self.right_child = right_child
         self.goal_state = goal_state
+        self.new_state = state
         
     def get_moves(self):
         
         self.records = [[],[]]
         
         if self.state.index(0) == 8:
-            self.records[0].append('right')
-            self.records[1].append(self.state[7])
+            self.records[0].append('right') #move
+            self.records[1].append(self.state[7]) # cost
                         
         elif self.state.index(0) == 0:
-            self.records[0].append('left')
-            self.records[1].append(self.state[1])
+            self.records[0].append('left') # move
+            self.records[1].append(self.state[1]) # cost
             
         else:
-            self.records[0].extend(('left','right'))
-            self.records[1].extend((self.state[self.state.index(0)+1], 
-                self.state[self.state.index(0)-1])) 
+            self.records[0].extend(('left','right')) #moves
+            self.records[1].extend((self.state[self.state.index(0)+1],self.state[self.state.index(0)-1])) #cost
     
     def get_heuristic_cost(self, heuristic_function):
         
@@ -76,21 +66,30 @@ class node:
             
         else:
             print 'There is something wrong with the distance function you entered.'
-                
+            
     def move_piece(self, move):
+                
+        new_node = self.state[:] # the [:] creates a new copy, while eliminating the pointer reference
         
-        if move in self.records[self.key][0]:
+        if move in self.records[0]:
             
             if move == 'left':
-                new_state = self.state
-                new_state[new_state.index(0) + 1], new_state[new_state.index(0)] = \
-                    new_state[new_state.index(0)], new_state[new_state.index(0) + 1]
-                return [1,2,3,8,0,4,7,6,5] #not returning the new state?
+                zero_idx = new_node.index(0)
+                item_idx = new_node.index(0) + 1
+                new_item = new_node[zero_idx]
+                new_zero_item = new_node[item_idx]
+                new_node[zero_idx] = new_zero_item
+                new_node[item_idx] = new_item
+                return new_node
                 
             else:
-                new_state[new_state.index(0)], new_state[new_state.index(0) + 1] = \
-                    new_state[new_state.index(0) - 1], new_state[new_state.index(0)]
-                return [1,2,3,8,0,4,7,6,5] #not returning the new state?
+                zero_idx = new_node.index(0)
+                item_idx = new_node.index(0) - 1
+                new_item = new_node[zero_idx]
+                new_zero_item = new_node[item_idx]
+                new_node[zero_idx] = new_zero_item
+                new_node[item_idx] = new_item
+                return new_node
                 
         else:
             return 'In the given state a {enter_move} is not possible.'.format(enter_move = move)
@@ -124,50 +123,42 @@ class queue:
         
     def reset_state(self, key):
         self.init_state = key
-
- 
+        
 class searchTreeSolver:
         
-    def __init__(self, node):
+    def __init__(self, node_init):
         
-        self.current_node = node
-        self.goal_state = node.goal_state
-        self.current_state = node.state
-        
+        self.root = node_init.state
+        self.current_node = node_init
         self.structure = {}
-        self.structure[0] = node.state
+        self.structure[0] = node_init.state
         self.key_counter = 1
+        self.goal_state = node_init.goal_state
         
-    def breadth_first(self):
-        #(self, key, state, goal_state, parent_node, left_child, right_child)
+    def breadth_first(self):  
         
-        if self.current_state != self.goal_state:
+        if self.current_node.state != self.current_node.goal_state:
                         
-            move =  self.current_node.records[0][self.current_node.records[1].index \
-                (min(self.current_node.records[1]))]
-                
-            state = self.current_node.move_piece(move)
-            
-            new_node = node(self.key_counter,state,self.goal_state,self.key_counter - 1,None,None)
-            print new_node.state
+            move =  self.current_node.records[0] \
+                [self.current_node.records[1].index(min(self.current_node.records[1]))]     
+            new_state = self.current_node.move_piece(move)          
+            new_node = node(self.key_counter,new_state,self.goal_state,self.key_counter - 1,None,None)
             self.current_node = new_node
-            
-            self.structure[self.key_counter] = self.current_node.state
+            self.structure[self.key_counter] = new_state
+            self.key_counter += 1
             
         else:
             return 'Omg this fucking things worked!'
-            
 
-            
 easy = [1,3,4,8,6,2,7,0,5]      
-goal = [1,2,3,8,0,4,7,6,5]             
+goal = [1,2,3,8,0,4,7,6,5] 
+
 test_node = node(0,easy,goal,None,None,None)
 test_node.get_moves()
-test_node.get_heuristic_cost('heuristic_function')
-
+test_node.get_heuristic_cost('manhattan_distance')
 test_tree = searchTreeSolver(test_node)
-test_tree.current_node.records
 test_tree.breadth_first()
+test_tree.structure
 
 import pandas as pd
 for k,v in test_tree.structure.items():
@@ -176,16 +167,24 @@ for k,v in test_tree.structure.items():
     print pd.DataFrame([[v[0],v[1],v[2]], \
                         [v[3],v[4],v[5]], \
                         [v[6],v[7],v[8]]]).to_string(index = False, header = False)
-    
 
 
+'''
+Returns
+
+Move Count:  0 
+
+1  3  4
+8  6  2
+7  0  5
 
 
+Move Count:  1 
 
-
-
-
-
+1  3  4
+8  6  2
+7  5  0
+'''
 
 
 
